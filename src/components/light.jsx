f1b0ced7966b90125d0411a343a461faa950f42b
d1y <chenhonzhou@gmail.com>
2019-10-15 22:01:52 +0800

    push

---
+++ src/components/light.jsx
@@ -0,0 +1,10 @@
+// create by @d1y
+import { list } from '../utils/colorui'
+import {
+  Button
+} from 'react-bootstrap'
+export default App = ()=> (
+  <div>
+    
+  </div>
+)
--- src/utils/colorui.js
+++ src/utils/colorui.js
@@ -3,9 +3,102 @@
  * @email [chenhonzhou@mail.com]
  * @create date 2019-10-15 15:28:27
  * @modify date 2019-10-15 15:28:27
- * @desc []
+ * @desc [ 颜色列表, 来自 colorui.css ]
 **/
 
 export default {
-  list: []
+  list: [
+    {
+      text: 'red',
+      textln: '嫣红',
+      col: '#e54d42'
+    },
+    {
+      text: 'red-light',
+      col: '#fadbd9'
+    },
+    {
+      text: 'orange',
+      textln: '桔橙',
+      col: '#f37b1d'
+    },
+    {
+      text: 'orange-light',
+      col: '#fde6d2'
+    },
+    {
+      text: 'yellow',
+      textln: '明黄',
+      col: '#fbbd08'
+    },
+    {
+      text: 'yellow-light',
+      col: '#fef2ce'
+    },
+    {
+      text: 'olive',
+      textln: '橄榄',
+      col: '#8dc63f'
+    },
+    {
+      text: 'olive-light',
+      col: '#e8f4d9'
+    },
+    {
+      text: 'olive-light',
+      col: '#d7f0db'
+    },
+    {
+      text: 'green',
+      col: '#39b54a'
+    },
+    {
+      text: 'green-light',
+      col: '#d2f1f0'
+    },
+    {
+      text: 'cyan',
+      col: '#1cbbb4'
+    },
+    {
+      text: 'cyan-light',
+      col: '#d2f1f0'
+    },
+    {
+      text: 'blue',
+      col: '#0081ff'
+    },
+    {
+      text: 'purple',
+      col: '#6739b6'
+    },
+    {
+      text: 'mauve',
+      col: '#9c26b0'
+    },
+    {
+      text: 'pink',
+      col: '#e03997'
+    },
+    {
+      text: 'brown',
+      col: '#a5673f'
+    },
+    {
+      text: 'gray',
+      col: '#8799a3'
+    },
+    {
+      text: 'black',
+      col: '#333'
+    },
+    {
+      text: 'gray',
+      col: '#aaa'
+    },
+    {
+      text: 'darkGray',
+      col: '#666'
+    }
+  ]
 }
